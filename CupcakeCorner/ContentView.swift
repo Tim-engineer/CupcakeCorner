cbe2f6e637e2a161161460700c3c9d761d50dc73
Tim-engineer <matlakt@outlook.com>
2024-06-18 16:37:24 +0200

    asyncimage

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -9,18 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
-            if let image = phase.image {
-                image
-                    .resizable()
-                    .scaledToFit()
-            } else if phase.error != nil {
-                Text("There was an error loading the image.")
-            } else {
-                ProgressView()
-            }
+        Form {
+            
         }
-        .frame(width: 200, height: 200)
     }
 }
 
