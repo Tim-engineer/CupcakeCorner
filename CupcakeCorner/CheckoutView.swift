d093e19ae83b2f85113cb14baf9d079993eb442e
Tim-engineer <matlakt@outlook.com>
2024-06-25 19:29:29 +0200

    working

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -10,11 +10,77 @@ import SwiftUI
 struct CheckoutView: View {
     var order: Order
     
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"), scale: 3) { image in
+                        image
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(RoundedRectangle(cornerRadius: 22))
+                            .padding(.horizontal)
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(height: 222)
+
+                Text("Your total is \(order.cost, format: .currency(code: "EUR"))")
+                    .font(.title3).bold()
+                    .foregroundStyle(.mint)
+                    .padding(.top)
+
+                Button {
+                    Task {
+                        await placeOrder()
+                    }
+                } label: {
+                    Text("Place Order")
+                }
+                .buttonStyle(.bordered)
+                .tint(.cyan)
+                .padding(.top)
+            }
+        }
+        .navigationTitle("Check out")
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollBounceBehavior(.basedOnSize)
+        .alert(alertTitle, isPresented: $showingAlert) { } message: {
+            Text(alertMessage)
+        }
+    }
+    
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            alertTitle = "Thank you!"
+            alertMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+            showingAlert = true
+        } catch {
+            print("Checkout failed: \(error.localizedDescription)")
+            alertTitle = "OOPS!"
+            alertMessage = "You are not connected to the internet. Try again later."
+            showingAlert = true
+        }
     }
 }
 
 #Preview {
-    CheckoutView(order: Order())
+    NavigationStack {
+        CheckoutView(order: Order())
+    }
 }
